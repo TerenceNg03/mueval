08fbbd75c0ae1dabbb15cc8c243b37c41f2b3d23
gwern0 <gwern0@gmail.com>
2008-07-20 10:05:46 -0400

    factor out main.hs messiness into Mueval.Concurrent
    We also try to refactor the multiple threads and watchdogs.
    
    darcs-hash:20080720140546-f7719-aa4d4108521c301cb64467341fb9cc27dcde98f6

---
+++ mueval/Mueval/Concurrent.hs
@@ -0,0 +1,45 @@
+module Mueval.Concurrent where
+
+import Control.Concurrent   (forkIO, killThread, myThreadId, threadDelay, throwTo, yield, ThreadId)
+import System.Posix.Signals (sigXCPU, installHandler, Handler(CatchOnce))
+import Control.Exception (catchDyn, Exception(ErrorCall))
+import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
+import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
+
+import Mueval.Interpreter
+import Mueval.ParseArgs
+
+-- | Fork off a thread which will sleep and kill off another thread at some point.
+watchDog :: Int -> ThreadId -> IO ()
+watchDog tout tid = do installHandler sigXCPU
+                                          (CatchOnce
+                                           $ throwTo tid $ ErrorCall "Time limit exceeded.") Nothing
+                       forkIO $ do threadDelay (tout * 1000000)
+                                   -- Time's up. It's a good day to die.
+                                   throwTo tid (ErrorCall "Time limit exceeded")
+                                   yield -- give the other thread a chance
+                                   killThread tid -- Die now, srsly.
+                                   error "Time expired"
+                       return () -- Never reached. Either we error out in
+                                 -- watchDog, or the evaluation thread finishes.
+
+-- | Set a watchdog on this thread, and then evaluate.
+forkedMain :: Options -> IO ()
+forkedMain opts = do
+  -- This *should* be redundant with the previous watchDog,
+  -- but maybe not.
+  mvar <- newEmptyMVar
+
+  myThreadId >>= watchDog tout
+
+  hSetBuffering stdout NoBuffering
+
+  -- Our modules and expression are set up. Let's do stuff.
+  forkIO (interpreterSession typeprint mdls expr `catchDyn` (printInterpreterError) >> putMVar mvar "Done.")
+
+  takeMVar mvar -- block until ErrorCall, or forkedMain succeeds
+  return ()
+          where mdls = modules opts
+                expr = expression opts
+                tout = timeLimit opts
+                typeprint = printType opts
--- mueval/main.hs
+++ mueval/main.hs
@@ -4,60 +4,22 @@
 -- <http://www.opengroup.org/onlinepubs/009695399/functions/setegid.html>
 module Main (main) where
 
-import Control.Concurrent   (forkIO, killThread, myThreadId, threadDelay, throwTo, yield, ThreadId)
-import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar, MVar)
-import Control.Exception (catchDyn, Exception(ErrorCall))
-import System.Environment (getArgs)
-import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
-import System.Posix.Signals (sigXCPU, installHandler, Handler(CatchOnce))
 
-import qualified Mueval.Context (cleanModules, unsafed)
-import Mueval.Interpreter
-import Mueval.ParseArgs
+import qualified Mueval.Context (cleanModules, unsafe)
+import Mueval.ParseArgs (getOptions, Options(..))
+import Mueval.Concurrent
 
 main :: IO ()
-main = do input <- getArgs
-          (opts,_) <- interpreterOpts input
-          if (Mueval.Context.cleanModules $ modules opts) then do
-              if (not $ Mueval.Context.unsafed $ expression opts) then do
-                                               mvar <- newEmptyMVar
+main = do opts <- getOptions
+          doIfSafe opts forkedMain
 
-                                               myThreadId >>= watchDog (timeLimit opts)
+-- We don't keep this in one of the other modules, because it's policy; other
+-- similar programs may not care.
+doIfSafe :: Options -> (Options -> t t1) -> t t1
+doIfSafe opts f = if (Mueval.Context.cleanModules $
+                            modules opts) then do
+                                            if (not $ Mueval.Context.unsafe $ expression opts) then
+                                               f opts
+                                             else error "Unsafe functions to use mentioned."
+                  else error "Unknown or untrusted module supplied! Aborting."
 
-                                               forkIO $ forkedMain (mvar) opts
-                                               takeMVar mvar -- block until a ErrorCall or the forkedMain succeeds
-
-                                               return ()
-               else error "Unsafe functions to use mentioned."
-           else error "Unknown or untrusted module supplied! Aborting."
-
--- Set a watchdog, and then evaluate.
-forkedMain :: MVar [Char] -> Options -> IO ()
-forkedMain mvar opts = do
-  -- This *should* be redundant with the previous watchDog,
-  -- but maybe not.
-
-  myThreadId >>= watchDog tout
-
-  hSetBuffering stdout NoBuffering
-
-  -- Our modules and expression are set up. Let's do stuff.
-  interpreterSession typeprint mdls expr `catchDyn` (printInterpreterError)
-  putMVar mvar "Done."
-          where mdls = modules opts
-                expr = expression opts
-                tout = timeLimit opts
-                typeprint = printType opts
-
--- | Fork off a thread which will sleep and kill off another thread at some point.
-watchDog :: Int -> ThreadId -> IO ()
-watchDog tout tid = do installHandler sigXCPU
-                                          (CatchOnce
-                                           $ throwTo tid $ ErrorCall "Time limit exceeded.") Nothing
-                       forkIO $ do threadDelay (tout * 1000000)
-                                   -- Time's up. It's a good day to die.
-                                   throwTo tid (ErrorCall "Time limit exceeded")
-                                   yield -- give the other thread a chance
-                                   killThread tid -- Die now, srsly.
-                                   error "Time expired"
-                       return ()
