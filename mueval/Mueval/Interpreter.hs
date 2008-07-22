93cf80a4da0b98f452362e425fee771b86f8d078
gwern0 <gwern0@gmail.com>
2008-07-21 20:57:01 -0400

    Interpreter.hs: catch exceptions from 'interpreter', so we can error out early without waiting for the watchdog threads
    darcs-hash:20080722005701-f7719-a2925c00b91bae72eea695b0857e6be548c9fa88

--- mueval/Mueval/Interpreter.hs
+++ mueval/Mueval/Interpreter.hs
@@ -1,14 +1,16 @@
 -- TODO: suggest the convenience functions be put into Hint proper?
 module Mueval.Interpreter (interpreterSession, printInterpreterError, ModuleName) where
 
+import Control.Monad.Trans (liftIO)
+import qualified Control.Exception (catch)
+
 import Language.Haskell.Interpreter.GHC (eval, newSession, reset, setImports,
                                          setOptimizations, setUseLanguageExtensions, setInstalledModsAreInScopeQualified,
                                                          typeChecks, typeOf, withSession,
                                          Interpreter, InterpreterError, ModuleName, Optimizations(All))
 
-import Control.Monad.Trans (liftIO)
-
 import qualified Mueval.Resources (limitResources)
+
 
 say :: String -> Interpreter ()
 say = liftIO . putStr . take 1024
@@ -37,4 +39,6 @@ interpreter prt modules expr = do
                                     else error "Expression did not type check."
 
 interpreterSession :: Bool -> [ModuleName] -> String -> IO ()
-interpreterSession prt mds expr = newSession >>= (flip withSession) (interpreter prt mds expr)
+interpreterSession prt mds expr = Control.Exception.catch
+                                  (newSession >>= (flip withSession) (interpreter prt mds expr))
+                                  (\_ -> error "Expression did not compile.")
