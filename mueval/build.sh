a564105a1fc4b42715c7bce3427ad0665921b1c6
gwern0 <gwern0@gmail.com>
2008-07-31 18:12:28 -0400

    build.sh: no point in testing multiple threads if first time around didn't work
    darcs-hash:20080731221228-f7719-725b359eff9ddbb1343711260fb446d7ebfc38d2

--- mueval/build.sh
+++ mueval/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Build
-runhaskell Setup configure --user && runhaskell Setup build && runhaskell Setup install || exit
-echo "\n...Single-threaded tests....\n"
-sh tests.sh
-echo "\n...Rerun the tests with multiple threads...\n"
-sh tests.sh +RTS -N4 -RTS --print-type
+(runhaskell Setup configure --user && runhaskell Setup build && runhaskell Setup install || exit) &&
+echo "\n...Single-threaded tests....\n" &&
+sh tests.sh &&
+echo "\n...Rerun the tests with multiple threads...\n" &&
+sh tests.sh +RTS -N4 -RTS
