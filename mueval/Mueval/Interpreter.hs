5591f0a8a93d7fe9615cb0e809a28e13ffab229c
Andrea Vezzosi <sanhziyan@gmail.com>
2008-08-20 10:38:36 -0400

    Display compiler errors and avoid masking exceptions
    darcs-hash:20080820143836-d32a2-a14b2eb166e15e6c7e029ebb103efec2d3f243a4

--- mueval/Mueval/Interpreter.hs
+++ mueval/Mueval/Interpreter.hs
@@ -2,19 +2,20 @@
 module Mueval.Interpreter where
 
 import Control.Monad (when)
-import qualified Control.Exception (catch)
+import qualified Control.Exception as E (bracket,catchDyn)
 import Control.Monad.Trans (liftIO)
 import System.Directory (copyFile, makeRelativeToCurrentDirectory, removeFile)
 import System.FilePath.Posix (takeFileName)
-
+import System.Exit (exitFailure)
 import Language.Haskell.Interpreter.GHC (eval, newSession, reset, setImports, loadModules,
                                          setOptimizations, setUseLanguageExtensions, setInstalledModsAreInScopeQualified,
-                                         typeChecks, typeOf, withSession, setTopLevelModules,
-                                         Interpreter, InterpreterError, ModuleName, Optimizations(All))
+                                         typeOf, withSession, setTopLevelModules,
+                                         Interpreter, InterpreterError(..),GhcError(..), ModuleName, Optimizations(All))
 
 
 import qualified Codec.Binary.UTF8.String as Codec (decodeString)
-import qualified System.IO.UTF8 as UTF (putStr)
+import qualified System.IO.UTF8 as UTF (putStrLn)
+
 
 import qualified Mueval.Resources (limitResources)
 
@@ -22,12 +23,27 @@ import qualified Mueval.Resources (limitResources)
 -- of interpreting something), but only print the first 1024 characters to avoid
 -- flooding. Lambdabot has a similar limit.
 say :: String -> Interpreter ()
-say = liftIO . UTF.putStr . Codec.decodeString . take 1024
+say = liftIO . sayIO
 
--- | Oh no, something has gone wrong. Call 'error' and then, as with 'say',
--- print out a maximum of 1024 characters.
+sayIO :: String -> IO ()
+sayIO = UTF.putStrLn . Codec.decodeString . take 1024
+
+-- | Oh no, something has gone wrong. If it's a compilation error prettyprint 
+-- the first 1024 chars of it and throw an ExitExcetion
+-- otherwise rethrow the exception in String form.
 printInterpreterError :: InterpreterError -> IO ()
-printInterpreterError = error . take 1024 . ("Oops... " ++) . show
+printInterpreterError (WontCompile errors) = 
+    -- if we get a compilation error we print it directly to avoid "mueval: .."
+    -- maybe it should go to stderr?
+    do sayIO $ concatMap (dropLinePosition . errMsg) errors
+       exitFailure
+    where 
+      -- each error starts with the line position, which is uninteresting
+      dropLinePosition = unlines . tail . lines
+-- other exceptions indicate some problem in mueval or the environment,
+-- so we rethrow them for debugging purpouses
+printInterpreterError other = error (show other)
+
 
 {- | The actual calling of Hint functionality. The heart of this just calls
    'eval', but we do so much more - we disable Haskell extensions, turn on
@@ -64,17 +80,14 @@ interpreter prt exts modules lfl expr = do
                                     Nothing -> return ()
                                     Just ms -> setImports ms
 
-                                  when prt (say $ expr ++ "\n")
+                                  when prt $ say expr
+                                  -- we don't check if the expression typechecks 
+                                  -- this way we get an InterpreterError we can display
+                                  when prt $ say =<< typeOf expr
 
-                                  checks <- typeChecks expr
+                                  result <- eval expr 
 
-                                  if checks then do
-                                              if prt then do say =<< typeOf expr
-                                                             say "\n"
-                                               else return ()
-                                              result <- eval expr
-                                              say $ result ++ "\n"
-                                    else error "Expression did not type check."
+                                  say $ result
 
 -- | Wrapper around 'interpreter'; supplies a fresh GHC API session and
 -- error-handling. The arguments are simply passed on.
@@ -86,13 +99,15 @@ interpreterSession :: Bool -- ^ Whether to print inferred type
                    -> String -- ^ The string to be interpreted as a Haskell expression
                    -> IO ()  -- ^ No real result, since printing is done deeper in
                             -- the stack.
-interpreterSession prt exts mds lfl expr = Control.Exception.catch
-                                  (newSession >>= (flip withSession) (interpreter prt exts mds lfl expr))
-                                  (\_ -> do case lfl of
-                                             "" -> return ()
-                                             l  -> do canonfile <- (makeRelativeToCurrentDirectory l)
-                                                      removeFile ("/tmp/" ++ takeFileName canonfile)
-                                            error "Expression did not compile.")
+interpreterSession prt exts mds lfl expr = E.bracket newSession cleanTmpFile $ \session ->
+                                  withSession session (interpreter prt exts mds lfl expr)
+                                  `E.catchDyn` printInterpreterError
+    where 
+      cleanTmpFile _ = case lfl of
+                         "" -> return ()
+                         l  -> do canonfile <- makeRelativeToCurrentDirectory l
+                                  removeFile $ "/tmp/" ++ takeFileName canonfile
+                                  
 
 mvload :: FilePath -> IO ()
 mvload lfl = do canonfile <- (makeRelativeToCurrentDirectory lfl)
