b4b9aa33950dac619cc350d96e5f46f4afe07fae
gwern0 <gwern0@gmail.com>
2008-08-21 15:21:26 -0400

    Resources.hs: restore resourcelimits
    darcs-hash:20080821192126-f7719-34e7bbfb3492e0dfcc3013d37ae410a8c3c5d169

--- mueval/Mueval/Resources.hs
+++ mueval/Mueval/Resources.hs
@@ -13,25 +13,25 @@ limitResources = do setCurrentDirectory "/tmp" -- will at least mess up relative
 
 -- | Set all the available rlimits.
 --   These values have been determined through trial-and-error
-stackSizeLimitSoft, stackSizeLimitHard,
- dataSizeLimitSoft,
+stackSizeLimitSoft, stackSizeLimitHard, totalMemoryLimitSoft, totalMemoryLimitHard,
+ dataSizeLimitSoft, openFilesLimitSoft, openFilesLimitHard, fileSizeLimitSoft, fileSizeLimitHard,
  dataSizeLimitHard, cpuTimeLimitSoft, cpuTimeLimitHard, coreSizeLimitSoft, coreSizeLimitHard, zero :: ResourceLimit
--- totalMemoryLimitSoft = dataSizeLimitSoft
--- totalMemoryLimitHard = dataSizeLimitHard
+totalMemoryLimitSoft = dataSizeLimitSoft
+totalMemoryLimitHard = dataSizeLimitHard
 -- These limits seem to be useless?
 stackSizeLimitSoft = zero
 stackSizeLimitHard = zero
 -- We allow a few files to be opened, such as package.conf, because they are necessary. This
 -- doesn't seem to be security problem because it'll be opened at the module
 -- stage, before code ever evaluates. I hope.
--- openFilesLimitSoft = openFilesLimitHard
--- openFilesLimitHard = ResourceLimit 7
+openFilesLimitSoft = openFilesLimitHard
+openFilesLimitHard = ResourceLimit 7
 -- TODO: It would be nice to set these to zero, but right now Hint gets around the
 -- insecurity of the GHC API by writing stuff out to a file in /tmp, so we need
 -- to allow our compiled binary to do file I/O... :( But at least we can still limit
 -- how much we write out!
--- fileSizeLimitSoft = fileSizeLimitHard
--- fileSizeLimitHard = ResourceLimit 590
+fileSizeLimitSoft = fileSizeLimitHard
+fileSizeLimitHard = ResourceLimit 10800
 dataSizeLimitSoft = dataSizeLimitHard
 dataSizeLimitHard = ResourceLimit $ 6^(12::Int)
 -- These should not be identical, to give the XCPU handler time to trigger
@@ -39,13 +39,15 @@ cpuTimeLimitSoft = ResourceLimit 4
 cpuTimeLimitHard = ResourceLimit 5
 coreSizeLimitSoft = coreSizeLimitHard
 coreSizeLimitHard = zero
+
+-- convenience
 zero = ResourceLimit 0
 
 limits :: [(Resource, ResourceLimits)]
 limits = [ (ResourceStackSize,    ResourceLimits stackSizeLimitSoft stackSizeLimitHard)
---         , (ResourceTotalMemory,  ResourceLimits totalMemoryLimitSoft totalMemoryLimitHard)
---         , (ResourceOpenFiles,    ResourceLimits openFilesLimitSoft openFilesLimitHard)
---         , (ResourceFileSize,     ResourceLimits fileSizeLimitSoft fileSizeLimitHard)
+         , (ResourceTotalMemory,  ResourceLimits totalMemoryLimitSoft totalMemoryLimitHard)
+         , (ResourceOpenFiles,    ResourceLimits openFilesLimitSoft openFilesLimitHard)
+         , (ResourceFileSize,     ResourceLimits fileSizeLimitSoft fileSizeLimitHard)
          , (ResourceDataSize,     ResourceLimits dataSizeLimitSoft dataSizeLimitHard)
          , (ResourceCoreFileSize, ResourceLimits coreSizeLimitSoft coreSizeLimitHard)
          , (ResourceCPUTime,      ResourceLimits cpuTimeLimitSoft cpuTimeLimitHard)]
