67599825339805eceace78f35d5f131862b4d7f7
gwern0 <gwern0@gmail.com>
2008-08-21 15:25:44 -0400

    +rlimits test in build.sh
    darcs-hash:20080821192544-f7719-f8e9b5df4b38df26b34b07866716eb70f1fb4119

--- mueval/build.sh
+++ mueval/build.sh
@@ -8,4 +8,6 @@ echo "\n...Single-threaded tests....\n" &&
 sh tests.sh &&
 echo "\n...Rerun the tests with multiple threads...\n" &&
 sh tests.sh +RTS -N4 -RTS &&
+echo "\n...Rerun tests with resource limits enabled...\n" &&
+sh tests.sh --rlimits &&
 echo "\n...Done, apparently everything worked!"
