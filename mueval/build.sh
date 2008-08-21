ed5df67060f95fd340be7e60e81ed8a466d450ab
gwern0 <gwern0@gmail.com>
2008-08-21 15:20:12 -0400

    fix build.sh to make abort if any tests do
    darcs-hash:20080821192012-f7719-45a4b27ce0469c028ffe6666ef9dc3d9311136c3

--- mueval/build.sh
+++ mueval/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+# Abort if any commands aren't successful
+set -e
 # Build
 (runhaskell Setup configure --user && runhaskell Setup build && runhaskell Setup install || exit) &&
+# Run the test suite with various options
 echo "\n...Single-threaded tests....\n" &&
 sh tests.sh &&
 echo "\n...Rerun the tests with multiple threads...\n" &&
-sh tests.sh +RTS -N4 -RTS
+sh tests.sh +RTS -N4 -RTS &&
+echo "\n...Done, apparently everything worked!"
