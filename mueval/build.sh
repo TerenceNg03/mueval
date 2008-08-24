af507b28de8c7cfe5a096bfe48329d9a179937b6
gwern0 <gwern0@gmail.com>
2008-08-24 00:24:53 -0400

    fix build.sh
    darcs-hash:20080824042453-f7719-7088dcabf6ef99f75a08136f90787ebdcb6a9bb9

--- mueval/build.sh
+++ mueval/build.sh
@@ -2,8 +2,7 @@
 # Abort if any commands aren't successful
 set -e
 # Build
-(runhaskell Setup configure --user && runhaskell Setup build && runhaskell Setup haddock
-    && runhaskell Setup install || exit) &&
+(runhaskell Setup configure --user && runhaskell Setup build && runhaskell Setup haddock && runhaskell Setup install || exit) &&
 # Run the test suite with various options
 echo "\n...Single-threaded tests....\n" &&
 sh tests.sh &&
