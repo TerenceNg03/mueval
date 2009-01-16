1b5f1f0c52fb4cfc51412fa2019b4f23bf62f425
gwern0 <gwern0@gmail.com>
2009-01-16 17:53:18 -0500

    +first draft of a process-level watchdog
    Ignore-this: 3700c6c3ab72af846698344bb77efa4a
    
    darcs-hash:20090116225318-f7719-fcbe3fd334386c976cb04210c6b2a3bc6b5c7cfc

---
+++ mueval/watchdog.hs
@@ -0,0 +1,21 @@
+-- | This implements a watchdog process. It calls mueval with all the
+--   user-specified arguments, sleeps, and then if mueval is still running
+--   kills it.
+--   Even an out-of-control mueval will have trouble avoiding 'terminateProcess'.
+--   Note that it's too difficult to parse the user arguments to get the timeout,
+--   so we specify it as a constant which is a little more generous than the default.
+module Main where
+
+import System.Environment
+import Control.Concurrent
+import System.Process
+import System.Exit
+
+main :: IO ()
+main = do args <- getArgs
+          hdl <- runProcess "mueval-core" args Nothing Nothing Nothing Nothing Nothing
+          threadDelay (7 * 700000)
+          status <- getProcessExitCode hdl
+          case status of 
+              Nothing -> terminateProcess hdl >> exitWith (ExitFailure 1)
+              Just a -> exitWith a
