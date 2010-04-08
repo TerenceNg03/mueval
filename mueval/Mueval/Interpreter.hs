c31e10e49999effff262869604f51afad5e5c7e3
Adam Vogt <vogt.adam@gmail.com>
2010-04-08 11:57:36 -0400

    Fix haddock parse error.
    Ignore-this: 80347deb8625bf9bbe5c2e1eec3b6279
    
    darcs-hash:20100408155736-1499c-cc311f5b28f3be7de8e0194992517f6e4736c4e2

--- mueval/Mueval/Interpreter.hs
+++ mueval/Mueval/Interpreter.hs
@@ -116,10 +116,10 @@ exceptionMsg :: String
 exceptionMsg = "*Exception: "
 
 -- | Renders the input String including its exceptions using @exceptionMsg@
-render :: (Control.Monad.Trans.MonadIO m) =>
-          Int -> -- ^ max number of characters to include
-          String -> -- ^ input
-          m (String, Bool) -- ^ ( output, @True@ if we found an exception )
+render :: (Control.Monad.Trans.MonadIO m)
+          => Int -- ^ max number of characters to include
+          -> String -- ^ input
+          -> m (String, Bool) -- ^ ( output, @True@ if we found an exception )
 render i xs =
     do (out,Any b) <- runWriterT $ render' i (toStream xs)
        return (out,b)
