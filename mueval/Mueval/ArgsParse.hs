b9edbe7bd4d5e7f36e98a2a961923654e24021ac
gwern0 <gwern0@gmail.com>
2010-06-01 13:29:20 -0400

    ArgsParse.hs: harmonize argument names
    Ignore-this: e5a3b54b7fbf11ad40210ae79744e11a
    
    darcs-hash:20100601172920-f7719-a62b2d82fc6d18c69d9aa31059cfae7624ddf1d4

--- mueval/Mueval/ArgsParse.hs
+++ mueval/Mueval/ArgsParse.hs
@@ -40,18 +40,18 @@ options :: [OptDescr (Options -> Options)]
 options = [Option "p"     ["password"]
                       (ReqArg (\u opts -> opts {user = u}) "PASSWORD")
                       "The password for the mubot account. If this is set, mueval will attempt to setuid to the mubot user. This is optional, as it requires the mubot user to be set up properly. (Currently a null-op.)",
-           Option "t"     ["timelimit"]
+           Option "t"     ["time-limit"]
                       (ReqArg (\t opts -> opts { timeLimit = read t :: Int }) "TIME")
                       "Time limit for compilation and evaluation",
 
-           Option "l"     ["loadfile"]
+           Option "l"     ["load-file"]
                       (ReqArg (\e opts -> opts { loadFile = e}) "FILE")
                       "A local file for Mueval to load, providing definitions. Contents are trusted! Do not put anything dubious in it!",
 
            Option "m"     ["module"]
                       (ReqArg (\m opts -> opts { modules = liftM (m:) (modules opts)}) "MODULE")
                       "A module we should import functions from for evaluation. (Can be given multiple times.)",
-           Option "n"     ["noimports"]
+           Option "n"     ["no-imports"]
                       (NoArg (\opts -> opts { noImports = True}))
                       "Whether to import any default modules, such as Prelude; this is useful if you are loading a file which, say, redefines Prelude operators.",
            Option "E"     ["Extensions"]
@@ -66,7 +66,7 @@ options = [Option "p"     ["password"]
            Option "i"     ["inferred-type"]
                       (NoArg (\opts -> opts { printType = True}))
                       "Whether to enable printing of inferred type and the expression (as Mueval sees it). Defaults to false.",
-           Option "r"     ["rlimits"]
+           Option "r"     ["resource-limits"]
                       (NoArg (\opts -> opts { rLimits = True}))
                       "Enable resource limits (using POSIX rlimits). Mueval does not by default since rlimits are broken on many systems.",
            Option "h" ["help"]
