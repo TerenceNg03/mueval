e6bc986ebb5e7e1ad83da026f88312f9ef40fcb4
gwern0 <gwern0@gmail.com>
2011-01-11 15:22:09 -0500

    watchdog: throw in a signal via `kill` to wayward children
    Ignore-this: 794f7325391495cae64cef1c523f5ae9
    
    darcs-hash:20110111202209-f7719-93b8714aac4b27f70b2791c9b5df12c68e755836

--- mueval/watchdog.hs
+++ mueval/watchdog.hs
@@ -6,10 +6,12 @@
 --   so we specify it as a constant which is a little more generous than the default.
 module Main where
 
-import System.Environment
-import Control.Concurrent
-import System.Process
-import System.Exit
+import Control.Concurrent (forkIO, threadDelay)
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(ExitFailure))
+import System.Posix.Signals (signalProcess)
+import System.Process (getProcessExitCode, runProcess, terminateProcess, waitForProcess)
+import System.Process.Internals (withProcessHandle, ProcessHandle__(OpenHandle))
 
 main :: IO ()
 main = do args <- getArgs
@@ -18,7 +20,11 @@ main = do args <- getArgs
                      threadDelay (7 * 700000)
                      status <- getProcessExitCode hdl
                      case status of 
-                         Nothing -> terminateProcess hdl >> exitWith (ExitFailure 1)
+                         Nothing -> do terminateProcess hdl
+                                       _ <- withProcessHandle hdl (\x -> case x of 
+                                                                      OpenHandle pid -> signalProcess 9 pid >> return (undefined, undefined)
+                                                                      _ -> return (undefined,undefined))
+                                       exitWith (ExitFailure 1)
                          Just a -> exitWith a
           stat <- waitForProcess hdl
           exitWith stat
